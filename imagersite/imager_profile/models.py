a73b550b67f505cf62b1480335c827fdc0deafcc
Kyle R <KyleRichVA@gmail.com>
2016-04-05 21:08:48 -0700

    Created django project and imager_profile app and model.

---
+++ imagersite/imager_profile/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import models
+
+
+class User_Profile(models.Model):
+    camera_model = models.CharField(max_length=300)
+    photo_type = models.CharField(max_length=250)
+    friends = models.ManyToManyField("self")
+    region = models.CharField(max_length=250)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE,)
+    is_active = models.BooleanField()
