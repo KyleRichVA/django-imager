8191c321e9e2a0e169a5a94fdf80128a40d48cb5
Kyle R <KyleRichVA@gmail.com>
2016-04-06 16:47:20 -0700

    setup the User_Profile properly for the app requirements.

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -4,6 +4,12 @@ from django.conf import settings
 from django.db import models
 
 
+class User_Manager(models.Manager):
+    def get_queryset(self):
+        return super(User_Manager, self).get_queryset().filter(
+            user__is_active=True)
+
+
 class User_Profile(models.Model):
     camera_model = models.CharField(max_length=300)
     photo_type = models.CharField(max_length=250)
@@ -11,4 +17,8 @@ class User_Profile(models.Model):
     region = models.CharField(max_length=250)
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE,)
-    is_active = models.BooleanField()
+    active = User_Manager()
+
+    @property
+    def is_active(self):
+        return self.user.is_active
