7d95599bbf33bfe3962f235889615d758aeae287
Kyle R <KyleRichVA@gmail.com>
2016-04-08 12:37:44 -0700

    started up testing for User_Profile

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -2,10 +2,6 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.db import models
-from django.dispatch import receiver
-from django.db.models.signals import post_save, pre_delete
-import logging
-logger = logging.getLogger(__name__)
 
 
 class User_Manager(models.Manager):
@@ -26,23 +22,3 @@ class User_Profile(models.Model):
     @property
     def is_active(self):
         return self.user.is_active
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_profile(sender, created, instance, **kwargs):
-    if created:
-        try:
-            profile = User_Profile(user=instance)
-            profile.save()
-        except ValueError:
-            msg = u'Unable to create User Profile for {}'.format(instance)
-            logger.error(msg)
-
-
-@receiver(pre_delete, sender=settings.AUTH_USER_MODEL)
-def remove_profile(sender, instance, **kwargs):
-    try:
-        instance.profile.delete()
-    except AttributeError:
-        msg = u"Could not delete {}. Maybe it doesn't exist?".format(instance)
-        logger.warn(msg)
--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from models import User_Profile
+from django.contrib.auth.models import User
+import factory
 
 # Create your tests here.
+
+
+class ProfileTestCase(TestCase):
+
+    def setUp(self):
+        self.user = UserFactory.create()
+        self.user.set_password(u'password')
+        self.user.save()
+
+    def test_foo(self):
+        import pdb; pdb.set_trace()
+        self.assertTrue(False)
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = u'Judy Hopps'
+    email = u'Judy@zpd.gov'
