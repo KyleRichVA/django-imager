594420af2da9e75497a18a1ffea227c6b751188e
Kyle R <KyleRichVA@gmail.com>
2016-04-07 21:18:35 -0700

    On user creation a user_profile is setup to it.

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class User_Manager(models.Manager):
@@ -16,9 +18,17 @@ class User_Profile(models.Model):
     friends = models.ManyToManyField("self")
     region = models.CharField(max_length=250)
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
-                                on_delete=models.CASCADE,)
+                                on_delete=models.CASCADE,
+                                related_name='profile')
     active = User_Manager()
 
     @property
     def is_active(self):
         return self.user.is_active
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile(sender, created, instance, **kwargs):
+    if created:
+        profile = User_Profile(user=instance)
+        profile.save()
