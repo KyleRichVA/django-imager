d5a27897c97f85d145bfdc9270a20c4de34a2ca6
Kyle R <KyleRichVA@gmail.com>
2016-04-10 17:19:38 -0700

    Working on creating test about User_Profile objects. testing issues.

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from models import User_Profile
+from models import User_Profile, User_Manager
 from django.contrib.auth.models import User
+from django.core.validators import MaxLengthValidator
 import factory
 
 # Create your tests here.
@@ -13,9 +14,29 @@ class ProfileTestCase(TestCase):
         self.user.set_password(u'password')
         self.user.save()
 
-    def test_foo(self):
-        import pdb; pdb.set_trace()
-        self.assertTrue(False)
+    def test_user_has_profile(self):
+        """Test that a django user has a profile connected to it."""
+        self.assertIsInstance(self.user.profile, User_Profile)
+
+    def test_profile_good(self):
+        """Test the profile has all its properties."""
+        long_str = u"A"*350
+        profile = self.user.profile
+        #friend = self.user.profile.friends.create()
+        self.assertFieldOutput(profile.camera_model,
+                               {u'A Camera': u'A Camera'},
+                               {long_str: ['max_length']})
+        self.assertFieldOutput(profile.photo_type,
+                               {u'Photos': u'Photos'},
+                               {long_str: MaxLengthValidator})
+        self.assertFieldOutput(profile.region,
+                               {u'USA': u'USA'},
+                               {long_str: MaxLengthValidator})
+        self.assertIsInstance(profile.user, User)
+        #self.assertIn(profile.friends, friend)
+        self.assertTrue(profile.is_active)
+        self.assertIsInstance(profile.active, User_Manager)
+        self.assertIn(User.profile.active.all(), profile)
 
 
 class UserFactory(factory.django.DjangoModelFactory):
