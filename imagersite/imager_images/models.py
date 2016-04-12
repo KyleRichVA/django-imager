c80da8ac6fb65ccd96ae8dbbd0a456e06b5b3738
Kyle R <KyleRichVA@gmail.com>
2016-04-11 22:32:06 -0700

    Changed over database to local postgresql

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -11,26 +11,26 @@ PUBLISH_CHOICES = (
     )
 
 
-class Album(models.Model):
-    owner = models.ForeignKey(User_Profile, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    cover = models.OneToOneField('Photo', on_delete=models.SET_DEFAULT,
-                                 default=None,
-                                 related_name='cover')
-    description = models.CharField(max_length=1000)
-    date_uploaded = models.DateField(auto_now_add=True)
-    date_updated = models.DateField(auto_now=True)
-    date_published = models.DateField()
-    published = models.CharField(max_length=6,
-                                 choices=PUBLISH_CHOICES,
-                                 default='PUBLIC')
+# class Album(models.Model):
+#     owner = models.ForeignKey(User_Profile, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=200)
+#     cover = models.OneToOneField('Photo', on_delete=models.SET_DEFAULT,
+#                                  default=None,
+#                                  related_name='cover')
+#     description = models.CharField(max_length=1000)
+#     date_uploaded = models.DateField(auto_now_add=True)
+#     date_updated = models.DateField(auto_now=True)
+#     date_published = models.DateField()
+#     published = models.CharField(max_length=6,
+#                                  choices=PUBLISH_CHOICES,
+#                                  default='PUBLIC')
 
 
 class Photo(models.Model):
     file = models.ImageField(upload_to='photos')
     owner = models.OneToOneField(User_Profile, on_delete=models.CASCADE)
-    album = models.ForeignKey(Album, on_delete=models.SET_DEFAULT,
-                              default=None)
+    # album = models.ForeignKey(Album, on_delete=models.SET_DEFAULT,
+    #                           default=None)
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     date_uploaded = models.DateField(auto_now_add=True)
